13bc542a38cc375bab2cb5c03318ed0e218dbb20
Christina Daskalaki <chdask@gmail.com>
2014-08-08 00:00:38 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+# Download & Start ElasticSearch
+
+echo =================
+echo "download"
+echo =================
+https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.1.tar.gz
+
+echo =================
+echo "extract"
+echo =================
+tar zxf elasticsearch-1.3.1.tar.gz
+cd elasticsearch-1.3.1
+
+echo
+echo =================
+echo "start @foreground"
+echo =================
+bin/elasticsearch 
+
+#start as a deamon:
+#bin/elasticsearch -d
+
+echo
+echo =================
+echo "test it's alive"
+echo =================
+curl localhost:9200
+
+#Expected output status: 200
