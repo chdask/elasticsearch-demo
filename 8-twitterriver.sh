539e00c5a2c1722c346ad936575010ff22324a2d
Christina Daskalaki <chdask@gmail.com>
2014-08-08 00:09:58 +0100

    Create 8-twitterriver.sh

---
+++ 8-twitterriver.sh
@@ -0,0 +1,24 @@
+# Twitter River 
+
+curl -XPUT localhost:9200/_river/frosconriver/_meta -d '
+{
+    "type" : "twitter",
+    "twitter" : {
+    	"type" : "sample",
+        "oauth" : {
+            "consumer_key" : "CONSUMER_KEY",
+            "consumer_secret" : "CONSUMER_SECRET",
+            "access_token" : "ACCESS_TOKEN",
+            "access_token_secret" : "ACCESS_TOKEN_SECRET"
+        },
+        "filter" : {
+            "tracks" : "text to track"
+        }
+    },
+    "index" : {
+        "index" : "index_name",
+        "type" : "tweet",
+        "bulk_size" : 1
+    }
+}
+'
