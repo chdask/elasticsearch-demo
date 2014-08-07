949d0c9aed7773319705e411e20c27bdbd786fce
Christina Daskalaki <chdask@gmail.com>
2014-08-07 22:22:26 +0100

    Update elasticsearch-setup.sh

--- elasticsearch-setup.sh
+++ elasticsearch-setup.sh
@@ -11,10 +11,33 @@ cd elasticsearch-1.3.1
 
 echo
 echo =================
-echo "start foreground"
+echo "start @foreground"
 echo =================
 bin/elasticsearch 
 
-#start as a deamon -d
+#start as a deamon:
+#bin/elasticsearch -d
 
-curl localhost:9200;
+echo
+echo =================
+echo "test it works"
+echo =================
+curl localhost:9200
+
+#Expected output:
+/*
+{
+  "status" : 200,
+  "name" : "Mephisto",
+  "version" : {
+    "number" : "1.3.1",
+    "build_hash" : "2de6dc5268c32fb49b205233c138d93aaf772015",
+    "build_timestamp" : "2014-07-28T14:45:15Z",
+    "build_snapshot" : false,
+    "lucene_version" : "4.9"
+  },
+  "tagline" : "You Know, for Search"
+}
+*/
+
+
