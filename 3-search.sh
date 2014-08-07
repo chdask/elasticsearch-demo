2a6234aa4d9c094fee82002d3ad878d0735e9e48
Christina Daskalaki <chdask@gmail.com>
2014-08-08 00:04:44 +0100

    Create 3-search.sh

---
+++ 3-search.sh
@@ -0,0 +1,44 @@
+# Search
+
+TODO
+
+
+echo
+echo =================
+echo "Search everything"
+echo =================
+curl -XPOST localhost:9200/_search?pretty
+
+echo
+echo =================
+echo "URI search"
+echo =================
+#put tags
+curl -XPUT localhost:9200/blog/posts/1 -d '{
+    "title": "Introduction to Elasticsearch",
+    "tags": ["elasticsearch", "new york"]
+}'
+
+#TODO explain
+curl localhost:9200/blog/_refresh
+curl 'localhost:9200/blog/posts/_search?q=elasticsearch&pretty'
+
+
+echo
+echo =================
+echo "JSON search"
+echo =================
+curl localhost:9200/_search?pretty -d '{
+  "query": {
+    "term": {
+      "tags": "new"
+    }
+  }
+}'
+
+###### Analyze API  ###### 
+echo
+echo =================
+echo "analyze API"
+echo =================
+curl localhost:9200/_analyze?pretty -d 'new york'
